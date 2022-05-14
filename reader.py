d97747f800106ac99ff2e3999e0327fcb7aa4a84
MehceUnisen <mehceu@gmail.com>
2022-05-14 16:41:20 +0300

    ACK怒ってはいけません

---
+++ reader.py
@@ -0,0 +1,27 @@
+#print("I love boobs")
+import pandas as pd
+import matplotlib.pyplot as plt
+
+heading_list = ["gnss", "msgLen", "year", "month","day", "hour", "min", "sec", "rtkStat","headingStat", "gpsStat", "gloStat","bdsStat", "baselineN", "baselineE","baselineU", "baselineNStd", "baselineEStd","baselineUStd", "heading", "gpsPitch", "gpsRoll","gpsSpeed", "velN", "velE", "velUp", "xigVx", "xigVy", "xigVz", "lat", "lon","roverHei", "ecefX", "ecefY", "ecefZ", "xigLat","xigLon", "xigAlt", "xigEcefX", "xigEcefY","xigEcefZ","secLat", "secLon","secAlt", "gpsWeekSec", "diffage", "speedHeading", "undulation","galStat"]
+
+df = pd.read_csv("am.csv", usecols=heading_list)
+#buraya kadar ortak kod buraya kadar elleme
+
+
+#birden fazla grafigi ayni anda gosterebilmek icin figure kullaniliyor
+#parametre olarak aldigi string pencerenin adi oluyor
+plot1 = plt.figure("terapistim anime kizlari gercek degil diyor nasil amk yanimda bir tane var su anda")
+plt.plot(df["rtkStat"])
+plt.plot(df["year"])
+#birden fazla veriyi tek grafikte gostermek icin bu sekilde arka arkaya
+#iki tane veriyi plot demek lazim
+
+#ayri bir pencerede yeni bir grafik acmak icin yeni bir tane figure tanimliyoruz
+plot2 = plt.figure("待って ください")
+plt.plot(df["month"])
+#sonra da vur plotu gec
+
+
+
+#bu satir ne olursa olsun en sonda kalacak
+plt.show()
