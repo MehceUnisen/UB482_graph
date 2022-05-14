178430451a06ae0877138439d645a27da2cd3b3c
MehceUnisen <mehceu@gmail.com>
2022-05-14 17:46:50 +0300

    minor fixes

--- reader.py
+++ reader.py
@@ -4,13 +4,13 @@ import matplotlib.pyplot as plt
 
 heading_list = ["gnss", "msgLen", "year", "month","day", "hour", "min", "sec", "rtkStat","headingStat", "gpsStat", "gloStat","bdsStat", "baselineN", "baselineE","baselineU", "baselineNStd", "baselineEStd","baselineUStd", "heading", "gpsPitch", "gpsRoll","gpsSpeed", "velN", "velE", "velUp", "xigVx", "xigVy", "xigVz", "lat", "lon","roverHei", "ecefX", "ecefY", "ecefZ", "xigLat","xigLon", "xigAlt", "xigEcefX", "xigEcefY","xigEcefZ","secLat", "secLon","secAlt", "gpsWeekSec", "diffage", "speedHeading", "undulation","galStat"]
 
-df = pd.read_csv("am.csv", usecols=heading_list)
+df = pd.read_csv("log.csv", usecols=heading_list)
 #buraya kadar ortak kod buraya kadar elleme
 
 
 #birden fazla grafigi ayni anda gosterebilmek icin figure kullaniliyor
 #parametre olarak aldigi string pencerenin adi oluyor
-plot1 = plt.figure("terapistim anime kizlari gercek degil diyor nasil amk yanimda bir tane var su anda")
+plot1 = plt.figure("terapistim anime kizlari gercek degil diyor nasil degil yanimda bir tane var su anda")
 plt.plot(df["rtkStat"])
 plt.plot(df["year"])
 #birden fazla veriyi tek grafikte gostermek icin bu sekilde arka arkaya
